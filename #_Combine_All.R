910a47b3b345d3003743d8b83f54bfab5d6f6d45
Charlene_MSI_AI <q2330416@gmail.com>
2024-11-18 05:59:59 +0800

    KGD_EPPK

--- #_Combine_All.R
+++ #_Combine_All.R
@@ -21,5 +21,15 @@ saveRDS(acral_melanoma, file = paste0(folder_path, "GSE215121_Acral_Melanoma_Seu
 GSE215121_Seurat <-  readRDS(file ="C:/Users/q2330/Dropbox/##_GitHub/##_KGD_Lab/Acral_Melanoma/Export/GSE215121_Acral_Melanoma_SeuratObject.rds")
 DimPlot(GSE215121_Seurat, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
 
+GSE215121_Seurat$Dataset <- "GSE215121"
+DimPlot(GSE215121_Seurat, reduction = "umap",group.by = "Dataset", label = TRUE, pt.size = 0.5) + NoLegend()
 
 
+#### KGD_EPPK ####
+load("C:/Users/q2330/Dropbox/##_GitHub/##_KGD_Lab/Acral_Melanoma/Export/#_KGL_Lab_EPPK_HS23378_2862.Temp.RData")
+# Save Seurat object
+saveRDS(seurat_obj, file = paste0(folder_path, "KGL_Lab_EPPK_HS23378_2862.rds"))
+KGD_EPPK__HS23378_2862_Seurat <-  readRDS(file ="C:/Users/q2330/Dropbox/##_GitHub/##_KGD_Lab/Acral_Melanoma/Export/KGL_Lab_EPPK_HS23378_2862.rds")
+DimPlot(KGD_EPPK__HS23378_2862_Seurat, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+KGD_EPPK__HS23378_2862_Seurat$Dataset <- "KGD_EPPK__HS23378_2862"
+DimPlot(KGD_EPPK__HS23378_2862_Seurat, reduction = "umap",group.by = "Dataset", label = TRUE, pt.size = 0.5) + NoLegend()
