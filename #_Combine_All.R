c23b6a92b3321b3cc9b614db78afc775256abe8c
Charlene_MSI_AI <q2330416@gmail.com>
2024-11-18 06:21:22 +0800

    Add GSE189889

--- #_Combine_All.R
+++ #_Combine_All.R
@@ -12,7 +12,23 @@ if(!require('Matrix')) install.packages("Matrix"); library(Matrix)
 Marker.lt <- c("EPGN", "EGFR", "KRT1", "KRT9", 
                "KIT", "EP300", "NF1", "COL1A1")
 
-#### GSE215121 ####
+
+
+#### KGD_EPPK ####
+# load("C:/Users/q2330/Dropbox/##_GitHub/##_KGD_Lab/Acral_Melanoma/Export/#_KGL_Lab_EPPK_HS23378_2862.Temp.RData")
+# # Save Seurat object
+# saveRDS(seurat_obj, file = paste0(folder_path, "KGL_Lab_EPPK_HS23378_2862.rds"))
+
+KGD_EPPK_HS23378_2862_Seurat <-  readRDS(file ="C:/Users/q2330/Dropbox/##_GitHub/##_KGD_Lab/Acral_Melanoma/Export/KGL_Lab_EPPK_HS23378_2862.rds")
+DimPlot(KGD_EPPK_HS23378_2862_Seurat, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+KGD_EPPK_HS23378_2862_Seurat$Dataset <- "KGD_EPPK_HS23378_2862"
+DimPlot(KGD_EPPK_HS23378_2862_Seurat, reduction = "umap",group.by = "Dataset", label = TRUE, pt.size = 0.5) + NoLegend()
+
+
+VlnPlot(KGD_EPPK_HS23378_2862_Seurat, features = Marker.lt, ncol = 4)
+
+
+#### GSE215121 Acral melanoma ####
 folder_path <- "C:/Users/q2330/Dropbox/##_GitHub/##_KGD_Lab/Acral_Melanoma/Export/"
 # load("C:/Users/q2330/Dropbox/##_GitHub/##_KGD_Lab/Acral_Melanoma/Export/GSE215121_Acral_Melanoma_Temp.RData")
 # DimPlot(acral_melanoma, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
@@ -29,18 +45,13 @@ DimPlot(GSE215121_Seurat, reduction = "umap",group.by = "Dataset", label = TRUE,
 VlnPlot(GSE215121_Seurat, features = Marker.lt, ncol = 4)
 
 
-#### KGD_EPPK ####
-# load("C:/Users/q2330/Dropbox/##_GitHub/##_KGD_Lab/Acral_Melanoma/Export/#_KGL_Lab_EPPK_HS23378_2862.Temp.RData")
-# # Save Seurat object
-# saveRDS(seurat_obj, file = paste0(folder_path, "KGL_Lab_EPPK_HS23378_2862.rds"))
-
-KGD_EPPK__HS23378_2862_Seurat <-  readRDS(file ="C:/Users/q2330/Dropbox/##_GitHub/##_KGD_Lab/Acral_Melanoma/Export/KGL_Lab_EPPK_HS23378_2862.rds")
-DimPlot(KGD_EPPK__HS23378_2862_Seurat, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-KGD_EPPK__HS23378_2862_Seurat$Dataset <- "KGD_EPPK__HS23378_2862"
-DimPlot(KGD_EPPK__HS23378_2862_Seurat, reduction = "umap",group.by = "Dataset", label = TRUE, pt.size = 0.5) + NoLegend()
+#### GSE189889 Acral melanoma ####
+GSE189889_Seurat <-  readRDS(file ="C:/Users/q2330/Dropbox/KGD_Lab/20241015 (已整理)(EPPK)(線上資料整理)_Acral Melanoma/Acral melanoma datasets/GSE189889_Acral_Melanoma_SeuratObject.rds")
+DimPlot(GSE189889_Seurat, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+GSE189889_Seurat$Dataset <- "GSE189889"
+DimPlot(GSE189889_Seurat, reduction = "umap",group.by = "Dataset", label = TRUE, pt.size = 0.5) + NoLegend()
 
 
-VlnPlot(KGD_EPPK__HS23378_2862_Seurat, features = Marker.lt, ncol = 4)
-
+VlnPlot(GSE189889_Seurat, features = Marker.lt, ncol = 4)
 
 
