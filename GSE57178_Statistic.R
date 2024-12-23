1760e1b135231a7dba9fea16d42bec5c7dfee52c
Charlene_MSI_AI <q2330416@gmail.com>
2024-12-23 15:54:51 +0800

    Checkpoint

--- GSE57178_Statistic.R
+++ GSE57178_Statistic.R
@@ -13,30 +13,31 @@ group_comparisons <- list(
 plot_data <- data  # 使用已經整理好的 data 數據框
 # 確認格式：plot_data 應該至少包含列 `Expression` 和 `Group`
 
-# 計算 logFC 和 p 值的函數
+
+# 計算均值差值和 p 值的函數
 calculate_stats <- function(data, marker, groups) {
   # 篩選數據
   data_filtered <- data %>%
     filter(Group %in% groups) %>%
     select(Group, Expression)
   
-  # 計算 logFC
+  # 計算均值差值
   group_means <- data_filtered %>%
     group_by(Group) %>%
     summarize(mean_expression = mean(Expression), .groups = 'drop') %>%
     arrange(match(Group, groups))
   
-  logFC <- log2(group_means$mean_expression[2] / group_means$mean_expression[1])
+  log2FC <- group_means$mean_expression[2] - group_means$mean_expression[1]
   
   # 計算 p 值 (t 檢驗)
   group1_values <- data_filtered %>% filter(Group == groups[1]) %>% pull(Expression)
   group2_values <- data_filtered %>% filter(Group == groups[2]) %>% pull(Expression)
   p_value <- t.test(group1_values, group2_values)$p.value
   
-  return(data.frame(Marker = marker, Group1 = groups[1], Group2 = groups[2], logFC = logFC, p_value = p_value))
+  return(data.frame(Marker = marker, Group1 = groups[1], Group2 = groups[2], log2FC = log2FC, p_value = p_value))
 }
 
-# 計算每個 Marker 的 logFC 和 p 值
+# 繼續分析和統計
 stats_results <- lapply(Marker.lt, function(marker) {
   do.call(rbind, lapply(group_comparisons, function(groups) {
     calculate_stats(plot_data, marker, groups)
